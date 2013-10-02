0a46d6621bf64aecdb486122942b7d8ae2223ce7
Matt Whitehead <matt_whitehead@bradv.com>
2013-10-01 22:20:30 -0500

    Updated the git ignore file.

--- public/js/tourApp/tour.js
+++ public/js/tourApp/tour.js
@@ -1,6 +1,6 @@
 $(function()
 {        
-    $.getJSON('tours/' + window.tourDirectory + '/config.json', function(config) // Get the config file for a specific tour, window.tourDirectory is set in mainView.php 
+    $.getJSON('tours/' + window.tourDirectory + '/config.json', function(config) // Get the config file for a specific tour, window.tourDirectory is set in mainView.php
     {
         document.title = config.pageTitle;
         $('head title').before('<link rel="stylesheet" href="public/css/tourApp/' + config.theme + '" media="screen" />'); // Inject the correct theme stylesheet into the head
